5ab72fa5468da86325c4e82388314b1f1f0fc8a7
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2020-05-11 13:23:10 +0530

    Create event.

--- app/src/main/java/com/example/campflow/JsonInterface.java
+++ app/src/main/java/com/example/campflow/JsonInterface.java
@@ -6,21 +6,24 @@ import com.example.campflow.ui.Posts.PostsClass;
 
 import java.util.ArrayList;
 
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 
 public interface JsonInterface {
 
-    @GET("b/5eb4039047a2266b14745cf7")
+    @GET("b/5eb8e675a47fdd6af161335f")
     Call<ArrayList<EventsNearMeClass>> getEvents();
 
     @Headers({"Content-Type:application/json","private:false"})
-    @POST("b/5eb4039047a2266b14745cf7")
-    Call<EventsNearMeClass> createEvent(@Header ("secret-key") String keyHeader,@Body EventsNearMeClass eventsNearMeClass);
+    @PUT("b/5eb8e675a47fdd6af161335f")
+    Call<ResponseBody> createEvent(@Header ("secret-key") String keyHeader, @Body RequestBody requestBody);
 
     @GET("b/5eb433aaa47fdd6af15f3873")
     Call<ArrayList<PoliciesForYouClass>> getPolicies();
